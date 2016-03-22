fe76b4d9b690873ec246ebe7451bce994fc78211
foster154 <foster154@gmail.com>
2016-03-22 08:39:50 -0600

    Initial commit

---
+++ Goals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Goals
+//
+//  Created by Mark F on 3/22/16.
+//  Copyright © 2016 Bunk Bed Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
